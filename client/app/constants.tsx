7f2c4860d0235cdf15767ed797cc3aafec87d742
nadavtal <nadavtalalmagor@gmail.com>
2025-05-05 11:00:20 +0300

    added main script

--- client/app/constants.tsx
+++ client/app/constants.tsx
@@ -3,7 +3,7 @@ export const DEVELOPMENT = 'development';
 console.log(process.env.NODE_ENV);
 export const isProduction = process.env.NODE_ENV === PRODUCTION;
 // export const isProduction = true;
-const url = isProduction ? 'https://www.picshareai.com/' : 'http://localhost:3000/';
+const url = isProduction ? 'https://www.picshareai.com/' : 'http://localhost:3001/';
 // const url = 'http://localhost:3000/';
 export const authApiUrl = url + 'api/users/';
 export const uploadsServer = url + 'api/uploads/';
